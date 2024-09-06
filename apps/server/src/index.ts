7d972d6534fef1436cc42c4bbe76da3b4eb2f825
mauryavinay1407 <vinaymaurya1407@gmail.com>
2024-09-06 15:09:52 +0530

    Feat: Socket added

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Heyy there");
+import http from 'http'
+import SocketService from './services/socket';
+
+async function init() {
+    const socketService=new SocketService();
+
+    const httpServer= http.createServer();
+    const PORT=process.env.PORT ? process.env.PORT : 8000;
+
+    socketService.io.attach(httpServer);
+
+    httpServer.listen(PORT,()=>
+    console.log(`Server is running at http://localhost:${PORT}`)  
+    );
+    socketService.initListeners();
+}
+init();
