e9f5842ded92cb10df53cc4b4c5adc29807abe99
mauryavinay1407 <vinaymaurya1407@gmail.com>
2024-09-08 22:54:50 +0530

    Context added

---
+++ apps/web/context/SockerProvider.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+
+interface SocketProviderProps {
+  children?: React.ReactNode;
+}
+
+interface ISocketContext {
+    sendMessage: (msg: string)=>any;
+}
+
+const SocketContext = React.createContext<ISocketContext | null>(null);
+
+export const SocketProvider: React.FC<SocketProviderProps> = ({children})=>{
+  const sendMessage: ISocketContext["sendMessage"]=useCallback((msg)=>{
+    console.log("Send Message",msg);    
+  },[])
+
+    return(
+        <SocketContext.Provider value={{ sendMessage }}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
